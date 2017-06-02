eb1b83bc4a1516a58b69d14924a95ae598ca2bea
zhaojh329 <jianhuizhao329@gmail.com>
2017-06-02 18:02:45 +0800

    Dev
    Signed-off-by: zhaojh329 <jianhuizhao329@gmail.com>

--- xterminal_broker.c
+++ xterminal_broker.c
@@ -8,67 +8,75 @@ void *memmem(const void *haystack, size_t haystacklen, const void *needle, size_
 static char *macaddr;
 static char *myid;
 
-static const char *s_address = "localhost:1883";
-struct termios oterm, nterm;
-
-static void ev_handler(struct mg_connection *nc, int ev, void *data)
+static const char *index_page = "<!DOCTYPE html><html><head>"
+	"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/><title>Websocket Test</title></head>"
+	"<body><button type=\"button\" onclick=\"connect()\">Connect</button>"
+	"<script>function connect(){var ws = new WebSocket(\"ws://\" + location.host);ws.onopen = function() {"
+	"console.log(\"connect ok\");ws.send(\"Hello WebSocket\");};ws.onmessage = function (evt) {"
+	"console.log(\"recv:\"+evt.data);};ws.onclose = function(){console.log(\"close\");};}</script></body></html>";
+	
+static void ev_handler(struct mg_connection *nc, int ev, void *ev_data)
 {
-	struct mg_mqtt_message *msg = (struct mg_mqtt_message *)data;
+	if (ev == MG_EV_CONNECT) {
+		/* Connect to mqtt broker */
+		struct mg_send_mqtt_handshake_opts opts;
+		memset(&opts, 0, sizeof(opts));
 
-	switch (ev) {
-		case MG_EV_CONNECT: {
-			struct mg_send_mqtt_handshake_opts opts;
-			memset(&opts, 0, sizeof(opts));
-
-			mg_set_protocol_mqtt(nc);
-			mg_send_mqtt_handshake_opt(nc, "xxxx12342", opts);
-			break;
+		mg_set_protocol_mqtt(nc);
+		mg_send_mqtt_handshake_opt(nc, "xxxx12342", opts);
+		
+	} else if (ev == MG_EV_MQTT_CONNACK) {
+		struct mg_mqtt_message *msg = (struct mg_mqtt_message *)ev_data;
+		struct mg_mqtt_topic_expression topic_expr[2];
+		
+		if (msg->connack_ret_code != MG_EV_MQTT_CONNACK_ACCEPTED) {
+			printf("Got mqtt connection error: %d\n", msg->connack_ret_code);
+			ev_break(EV_DEFAULT, EVBREAK_ALL);
+			return;
 		}
 
-		case MG_EV_MQTT_CONNACK: {
-			struct mg_mqtt_topic_expression topic_expr[2];
-			
-			if (msg->connack_ret_code != MG_EV_MQTT_CONNACK_ACCEPTED) {
-				printf("Got mqtt connection error: %d\n", msg->connack_ret_code);
-				ev_break(EV_DEFAULT, EVBREAK_ALL);
-				return;
-			}
+		topic_expr[0].qos = MG_MQTT_QOS(0);
+		asprintf((char **)&topic_expr[0].topic, "xterminal/%s/response/data/%s", macaddr, myid);
+		
+		topic_expr[1].qos = MG_MQTT_QOS(0);
+		asprintf((char **)&topic_expr[1].topic, "xterminal/%s/response/exit/%s", macaddr, myid);
+		
+		mg_mqtt_subscribe(nc, topic_expr, 2, 0);
 
-			topic_expr[0].qos = MG_MQTT_QOS(0);
-			asprintf((char **)&topic_expr[0].topic, "xterminal/%s/response/data/%s", macaddr, myid);
-			
-			topic_expr[1].qos = MG_MQTT_QOS(0);
-			asprintf((char **)&topic_expr[1].topic, "xterminal/%s/response/exit/%s", macaddr, myid);
-			
-			mg_mqtt_subscribe(nc, topic_expr, 2, 0);
-
-			free((char *)topic_expr[0].topic);
-			free((char *)topic_expr[1].topic);
-			
-			break;
+		free((char *)topic_expr[0].topic);
+		free((char *)topic_expr[1].topic);
+		
+	} else if (ev == MG_EV_MQTT_SUBACK) {
+		char topic[128] = "";
+		sprintf(topic, "xterminal/%s/request/connect/%s", macaddr, myid);
+		mg_mqtt_publish(nc, topic, 0, MG_MQTT_QOS(0), NULL, 0);
+		
+	} else if (ev == MG_EV_MQTT_PUBLISH) {
+		struct mg_mqtt_message *msg = (struct mg_mqtt_message *)ev_data;
+		if (memmem(msg->topic.p, msg->topic.len, "data", strlen("data"))) {
+			int ret = write(STDOUT_FILENO, msg->payload.p, msg->payload.len);
+			if (ret < 0)
+				perror("write");
+		} else if (memmem(msg->topic.p, msg->topic.len, "exit", strlen("exit"))) {
+			ev_break(EV_DEFAULT, EVBREAK_ALL);
 		}
-
-		case MG_EV_MQTT_SUBACK: {
-			char topic[128] = "";
-			sprintf(topic, "xterminal/%s/request/connect/%s", macaddr, myid);
-			mg_mqtt_publish(nc, topic, 0, MG_MQTT_QOS(0), NULL, 0);
-			break;
-		}
-
-		case MG_EV_MQTT_PUBLISH: {
-			if (memmem(msg->topic.p, msg->topic.len, "data", strlen("data"))) {
-				int ret = write(STDOUT_FILENO, msg->payload.p, msg->payload.len);
-				if (ret < 0)
-					perror("write");
-			} else if (memmem(msg->topic.p, msg->topic.len, "exit", strlen("exit"))) {
-				tcsetattr(0, TCSANOW, &oterm);
-				ev_break(EV_DEFAULT, EVBREAK_ALL);
-			}
-			break;
-		}
-		case MG_EV_CLOSE:
-	      printf("Connection closed\n");
-	      exit(1);
+			
+	} else if (ev == MG_EV_HTTP_REQUEST) {
+		mg_printf(nc, "HTTP/1.1 200 OK\r\n"
+							  "Content-Length: %d\r\n\r\n"
+							  "%s", (int)strlen(index_page), index_page);
+		
+	} else if (ev == MG_EV_WEBSOCKET_HANDSHAKE_DONE) {
+		char buf[] = "Hello Websocket Client";
+		printf("New websocket connection:%p\n", nc);
+		mg_send_websocket_frame(nc, WEBSOCKET_OP_TEXT, buf, strlen(buf));
+	
+	} else if (ev == MG_EV_WEBSOCKET_FRAME) {
+		struct websocket_message *wm = (struct websocket_message *)ev_data;
+		printf("recv websocket:%.*s\n", (int)wm->size, wm->data);
+		
+	} else if (ev == MG_EV_CLOSE) {
+		printf("Connection closed\n");
 	}
 }
 
@@ -77,20 +85,6 @@ static void signal_cb(struct ev_loop *loop, ev_signal *w, int revents)
 	printf("Got signal: %d\n", w->signum);
 	ev_break(loop, EVBREAK_ALL);
 }
-
-static void stdin_read_cb(struct ev_loop *loop, ev_io *w, int revents)
-{
-	int len = 0;
-	char buf[1024] = "", topic[128] = "";
-	struct mg_connection *nc = (struct mg_connection *)w->data;
-	
-	len = read(w->fd, buf, sizeof(buf));
-	if (len > 0) {
-		sprintf(topic, "xterminal/%s/request/data/%s", macaddr, myid);
-		mg_mqtt_publish(nc, topic, 0, MG_MQTT_QOS(0), buf, len);
-	}
-}
-
 		
 static void usage(const char *name)
 {
@@ -109,8 +103,9 @@ int main(int argc, char *argv[])
 	struct ev_loop *loop = EV_DEFAULT;
 	ev_signal sig_watcher;
 	struct mg_mgr mgr;
-	ev_io stdin_watcher;
 	struct mg_connection *nc;
+	const char *s_address = "localhost:1883";
+	const char *s_http_port = "8000";
 	int ch;
 
 	while ((ch = getopt(argc, argv, "m:i:")) != -1) {
@@ -141,20 +136,14 @@ int main(int argc, char *argv[])
 	if (!nc) {
 		fprintf(stderr, "mg_connect(%s) failed\n", s_address);
 	}
-
-	ev_io_init(&stdin_watcher, stdin_read_cb, 0, EV_READ);
-	stdin_watcher.data = nc;
-	ev_io_start(loop, &stdin_watcher);
-
-	tcgetattr(0, &oterm);
-	tcgetattr(0, &nterm);
-	nterm.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
-	nterm.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN);
-	nterm.c_lflag |=  ISIG;
-	nterm.c_cc[VMIN] = 1;
-	nterm.c_cc[VTIME] = 0;
-	nterm.c_cc[VINTR] = 1;
-	tcsetattr(0, TCSANOW, &nterm);
+	
+	nc = mg_bind(&mgr, s_http_port, ev_handler);
+	if (nc == NULL) {
+		printf("Failed to create listener\n");
+		return 1;
+	}
+	
+	mg_set_protocol_http_websocket(nc);
 	
 	ev_run(loop, 0);
 
