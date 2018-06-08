2d32d7fcb8a4d6695b6386c27d47f881468f8cac
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-06-08 13:54:47 +0800

    tools/install.sh: Check if libubox and ustream-ssl exist.
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- tools/install.sh
+++ tools/install.sh
@@ -108,7 +108,12 @@ check_tool cmake
 check_tool git
 check_tool quilt
 
+LIBUBOX_FOUND=
+USTREAM_SSL_FOUND=
+
 check_lib_header json.h json-c > /dev/null || $INSTALL $PKG_JSON
+check_lib_header uloop.h libubox > /dev/null && LIBUBOX_FOUND=1
+check_lib_header ustream-ssl.h libubox > /dev/null && USTREAM_SSL_FOUND=1
 
 LIBSSL_INCLUDE=$(check_lib_header opensslv.h)
 [ -n "$LIBSSL_INCLUDE" ] || {
@@ -120,36 +125,41 @@ rm -rf /tmp/rtty-build
 mkdir /tmp/rtty-build
 pushd /tmp/rtty-build
 
-git clone https://git.openwrt.org/project/libubox.git
-git clone https://git.openwrt.org/project/ustream-ssl.git
 git clone https://github.com/zhaojh329/libuwsc.git
 git clone https://github.com/zhaojh329/rtty.git
 
 # libubox
-cd libubox && cmake -DBUILD_LUA=OFF . && make install && cd -
-[ $? -eq 0 ] || exit 1
+[ -n "$LIBUBOX_FOUND" ] || {
+	git clone https://git.openwrt.org/project/libubox.git
+	cd libubox && cmake -DBUILD_LUA=OFF . && make install && cd -
+	[ $? -eq 0 ] || exit 1
+}
+
 
 # ustream-ssl
-cd ustream-ssl
-git checkout 189cd38b4188bfcb4c8cf67d8ae71741ffc2b906
+[ -n "$USTREAM_SSL_FOUND" ] || {
+	git clone https://git.openwrt.org/project/ustream-ssl.git
+	cd ustream-ssl
+	git checkout 189cd38b4188bfcb4c8cf67d8ae71741ffc2b906
+	LIBSSL_VER=$(cat $LIBSSL_INCLUDE/opensslv.h | grep OPENSSL_VERSION_NUMBER | grep -o '[0-9x]\+')
+	LIBSSL_VER=$(echo ${LIBSSL_VER:2:6})
 
-LIBSSL_VER=$(cat $LIBSSL_INCLUDE/opensslv.h | grep OPENSSL_VERSION_NUMBER | grep -o '[0-9x]\+')
-LIBSSL_VER=$(echo ${LIBSSL_VER:2:6})
+	# > 1.1
+	if [ $LIBSSL_VER -ge 101000 ];
+	then
+		quilt import ../libumqtt/tools/us-openssl_v1_1.patch
+		quilt push -a
+	elif [ $LIBSSL_VER -le 100020 ];
+	then
+	    # < 1.1.2
+	    quilt import ../libumqtt/tools/us-openssl_v1_0_1.patch
+	    quilt push -a
+	fi
 
-# > 1.1
-if [ $LIBSSL_VER -ge 101000 ];
-then 
-	quilt import ../rtty/tools/us-openssl_v1_1.patch
-	quilt push -a
-elif [ $LIBSSL_VER -le 100020 ];
-then
-    # < 1.1.2
-    quilt import ../rtty/tools/us-openssl_v1_0_1.patch
-    quilt push -a
-fi
+	cmake . && make install && cd -
+	[ $? -eq 0 ] || exit 1
+}
 
-cmake . && make install && cd -
-[ $? -eq 0 ] || exit 1
 
 # libuwsc
 cd libuwsc && cmake . && make install && cd -
