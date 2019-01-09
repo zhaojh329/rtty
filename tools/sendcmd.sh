93d0974420ce11e3994d04b06252676c0d83b700
Jianhui Zhao <jianhuizhao329@gmail.com>
2019-01-09 21:45:32 +0800

    Fix sendcmd.sh
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- tools/sendcmd.sh
+++ tools/sendcmd.sh
@@ -8,8 +8,8 @@ params='["hello rtty"]'
 username="test"
 password="test"
 
-resp=$(curl "http://$host:$port/cmd" -d "{\"devid\":\"$devid\", \"cmd\":\"$cmd\", \"params\": $params, \"username\": \"$username\", \"password\": \"$password\"}" 2>/dev/null)
-
+resp=$(curl -k "https://$host:$port/cmd" -d "{\"devid\":\"$devid\", \"cmd\":\"$cmd\", \"params\": $params, \"username\": \"$username\", \"password\": \"$password\"}" 2>/dev/null)
+echo "1:$resp"
 token=$(echo "$resp" | jq -r '.token')
 
 [ "$token" = "null" ] && {
@@ -20,7 +20,8 @@ token=$(echo "$resp" | jq -r '.token')
 
 while [ true ]
 do
-    resp=$(curl "http://$host:$port/cmd?token=$token" 2>/dev/null)
+    resp=$(curl -k "https://$host:$port/cmd?token=$token" 2>/dev/null)
+    echo "2:$resp"
     err=$(echo "$resp" | jq -r '.err')
     [ "$err" = "1005" ] && {
         echo "Pending..."
@@ -28,7 +29,7 @@ do
         continue
     }
 
-    [ -n "err" ] && {
+    [ "$err" != "null" ] && {
         msg=$(echo "$resp" | jq -r '.msg')
         echo "err: $err"
         echo "msg: $msg"
