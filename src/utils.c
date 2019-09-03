877145260fce389b9f2bb4b6e4ac990842134189
Jianhui Zhao <zhaojh329@gmail.com>
2019-09-03 14:54:23 +0800

    Change the license information
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2019 Jianhui Zhao <jianhuizhao329@gmail.com>
+ * Copyright (c) 2019 Jianhui Zhao <zhaojh329@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
